6b876e5ac72f6b8aa147dce16b35c563399c49bf
Gabriel Luis <gabriel.luis.cinza@gmail.com>
2018-02-01 20:57:10 -0200

    remove unused code

--- main.js
+++ main.js
@@ -3,7 +3,5 @@ document.body.querySelectorAll('form').forEach(form => {
   form.addEventListener("submit", (event) => {
     event.preventDefault()
     alert("Oh oh, you didn't say the magic word")
-
-    return false
   })
 })
