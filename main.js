18087ad29dbd28c8bec191471ab8fe3764dec32d
Gabriel Luis <gabriel.luis.cinza@gmail.com>
2018-02-09 22:09:36 -0200

    make it evil(er)

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ document.body.querySelectorAll("form").forEach(form => {
   form.addEventListener("submit", (event) => {
     event.preventDefault()
     alert("Oh oh, you didn't say the magic word")
+    window.location.reload()
   })
 })
